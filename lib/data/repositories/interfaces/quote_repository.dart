02719c0708b9b4a33b613001f54688678e4be51e
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-11-27 08:37:56 +0530

    feat: add lib/data/repositories/interfaces/quote_repository.dart

---
+++ lib/data/repositories/interfaces/quote_repository.dart
@@ -0,0 +1,20 @@
+import 'package:dev_quotes/core/error/failures.dart';
+import 'package:dev_quotes/core/utils/type_defs.dart';
+import 'package:dev_quotes/data/models/quote_model.dart';
+
+abstract class QuoteRepository {
+  Future<Result<Quote>> getRandomQuote();
+  Future<Result<List<Quote>>> searchQuotes(
+    String query, {
+    String? category,
+    String? author,
+  });
+  Future<Result<void>> addQuote(Quote quote);
+  Future<Result<void>> updateQuote(Quote quote);
+  Stream<List<Quote>> getFavorites(String userId);
+  Stream<List<Quote>> getUserQuotes(String userId);
+  Stream<List<Quote>> getPublicQuotes();
+  Future<Result<void>> addFavorite(String quoteId, String userId);
+  Future<Result<void>> removeFavorite(String quoteId, String userId);
+  Future<Result<List<Quote>>> getQuotesByCategory(String categoryId);
+}
