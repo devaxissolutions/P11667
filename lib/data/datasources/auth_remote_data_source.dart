4c0b53ed5555f9ca337e695b95f2238e6dd13cd4
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-11-27 08:37:56 +0530

    feat: add lib/data/datasources/auth_remote_data_source.dart

---
+++ lib/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+abstract class AuthRemoteDataSource {
+  Future<UserCredential> login(String email, String password);
+  Future<UserCredential> signup(String email, String password);
+  Future<void> logout();
+  User? get currentUser;
+  Future<UserCredential> signInWithGoogle();
+  Future<void> resetPassword(String email);
+  Stream<User?> get authStateChanges;
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  AuthRemoteDataSourceImpl({
+    FirebaseAuth? firebaseAuth,
+    GoogleSignIn? googleSignIn,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+       _googleSignIn =
+           googleSignIn ??
+           GoogleSignIn(
+             serverClientId:
+                 '958725673026-q173iglhhef0av7tbo402uttr61sc7d2.apps.googleusercontent.com',
+           );
+
+  @override
+  Future<UserCredential> login(String email, String password) {
+    return _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  Future<UserCredential> signup(String email, String password) {
+    return _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  Future<void> logout() async {
+    await _googleSignIn.signOut();
+    await _firebaseAuth.signOut();
+  }
+
+  @override
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  @override
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  @override
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+    if (googleUser == null) {
+      throw FirebaseAuthException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    // Create a new credential
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await _firebaseAuth.signInWithCredential(credential);
+  }
+
+  @override
+  Future<void> resetPassword(String email) {
+    return _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+}
