2be3822bdee05ba6f57bebdb01f725e3a69c947b
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-11-27 08:37:56 +0530

    feat: add lib/data/repositories/interfaces/auth_repository.dart

---
+++ lib/data/repositories/interfaces/auth_repository.dart
@@ -0,0 +1,13 @@
+import 'package:dev_quotes/core/error/failures.dart';
+import 'package:dev_quotes/core/utils/type_defs.dart';
+import 'package:dev_quotes/data/models/user_model.dart';
+
+abstract class AuthRepository {
+  Future<Result<User>> login(String email, String password);
+  Future<Result<User>> signup(String email, String password, String username);
+  Future<Result<void>> logout();
+  Future<Result<User>> getCurrentUser();
+  Future<Result<User>> signInWithGoogle();
+  Future<Result<void>> resetPassword(String email);
+  Stream<User?> get authStateChanges;
+}
