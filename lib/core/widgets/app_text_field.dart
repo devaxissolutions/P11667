31e6eb02b7d614d89d1206169e67f5cc04ce283b
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-11-27 08:37:56 +0530

    feat: add lib/core/widgets/app_text_field.dart

---
+++ lib/core/widgets/app_text_field.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../theme/colors.dart';
+import '../theme/typography.dart';
+
+class AppTextField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController? controller;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final ValueChanged<String>? onChanged;
+
+  const AppTextField({
+    super.key,
+    required this.hintText,
+    this.controller,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      validator: validator,
+      onChanged: onChanged,
+      style: AppTypography.body1,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: AppTypography.body2.copyWith(color: AppColors.textSecondary),
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+        filled: true,
+        fillColor: AppColors.surface,
+        contentPadding: const EdgeInsets.all(16),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(color: AppColors.divider),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(color: AppColors.divider),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(color: AppColors.primary),
+        ),
+      ),
+    );
+  }
+}
