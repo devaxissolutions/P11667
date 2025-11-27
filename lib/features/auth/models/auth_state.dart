07881953ac2e64e8ad40af2c9b7836a01669ee9f
Anonymous <53265299+shivamksharma@users.noreply.github.com>
2025-11-27 08:37:57 +0530

    feat: add lib/features/auth/models/auth_state.dart

---
+++ lib/features/auth/models/auth_state.dart
@@ -0,0 +1,29 @@
+import 'package:dev_quotes/data/models/user_model.dart';
+
+sealed class AuthState {
+  const AuthState();
+}
+
+class AuthInitial extends AuthState {
+  const AuthInitial();
+}
+
+class AuthLoading extends AuthState {
+  const AuthLoading();
+}
+
+class AuthAuthenticated extends AuthState {
+  final User user;
+
+  const AuthAuthenticated(this.user);
+}
+
+class AuthUnauthenticated extends AuthState {
+  const AuthUnauthenticated();
+}
+
+class AuthError extends AuthState {
+  final String message;
+
+  const AuthError(this.message);
+}
